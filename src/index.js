523a20498f04bee3ddf99dfa77b501165f9ae910
Max Strokov <maxorrs@gmail.com>
2020-09-28 23:50:42 +0300

    Создает компонент главной страницы

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
+
+const Setting = {
+  OFFERS_COUNT: 123
+};
+
+ReactDOM.render(
+    <App
+      offersCount={Setting.OFFERS_COUNT}
+    />,
+    document.getElementById(`root`)
+);
